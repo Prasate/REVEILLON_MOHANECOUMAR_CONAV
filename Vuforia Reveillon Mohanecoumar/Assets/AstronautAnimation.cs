7d70c3192dda1ecf977f0308a2471485fdfc06d2
Prasate <eric_em77@msn.com>
2019-03-29 12:42:12 +0100

    .

---
+++ Vuforia Reveillon Mohanecoumar/Assets/AstronautAnimation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronautAnimation : MonoBehaviour {
+
+
+    private Animator m_Animator;
+    private bool drill = false;
+    private bool wave = false;
+    private bool idle = false;
+
+    // Use this for initialization
+    void Start () {
+        m_Animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Toggle Drilling");
+            wave = false;
+            m_Animator.SetBool("IsWaving", wave);
+            drill = !drill;
+            m_Animator.SetBool("IsDrilling", drill);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Waving");
+            drill = false;
+            m_Animator.SetBool("IsDrilling", drill); m_Animator.SetBool("IsWaving", wave);
+            wave = !wave;
+            m_Animator.SetBool("IsWaving", wave);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Resetting");
+            wave = false;
+            drill = false;
+            m_Animator.SetBool("IsWaving", wave);
+            m_Animator.SetBool("IsDrilling", drill);
+        }
+    }
+}
---
+++ Vuforia Reveillon Mohanecoumar/Assets/DroneAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAnimation : MonoBehaviour {
+
+
+    private Animator m_Animator;
+    private bool scan = false;
+    private bool laser = false;
+    
+
+
+    // Use this for initialization
+    void Start () {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Toggle Scanning");
+            laser = false;
+            m_Animator.SetBool("IsShowingLaser", laser);
+            scan = !scan;
+            m_Animator.SetBool("IsScanning", scan);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("ShowingLaser");
+            scan = false;
+            m_Animator.SetBool("IsScanning", scan); m_Animator.SetBool("IsShowingLaser", laser);
+            laser = !laser;
+            m_Animator.SetBool("IsShowingLaser", laser);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Resetting");
+            laser = false;
+            scan = false;
+            m_Animator.SetBool("IsShowingLaser", laser);
+            m_Animator.SetBool("IsScanning", scan);
+        }
+    }
+}
